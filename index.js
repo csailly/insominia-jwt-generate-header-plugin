acc6e863e41883265adef0877602400e8b8a673c
Christophe S <christophe@sailly.fr>
2024-11-27 09:15:53 +0100

    fix; fix missing parameters on getPayload func

--- index.js
+++ index.js
@@ -1,55 +1,59 @@
 const jwt = require('jsonwebtoken');
 
 const PLUGIN_NAME = 'jwt-generate-header';
-const PLUGIN_ENVIRONMENT_SETTINGS = 'jwt-generate-header';
-const JWT_PAYLOAD_HEADER_NAME = 'jwt-generate-header-payload'; //@deprecated
 
-function cleanPluginHeader(context) {
-  context.request.removeHeader(JWT_PAYLOAD_HEADER_NAME);
+function cleanHeaders(context) {
+  context.request.removeHeader('jwt-generate-header-payload');
 }
 
-function getPayload() {
+function getPayload(context, jwtHeaderName) {
   return (
     context.request.getHeader(jwtHeaderName) ??
     context.request.getHeader(JWT_PAYLOAD_HEADER_NAME)
   );
 }
 
-module.exports.requestHooks = [
-  async context => {
-    console.log(`${PLUGIN_NAME} Running`);
+module.exports = {
+  name: PLUGIN_NAME,
+  displayName: 'JwtHeaderGenerator',
+  description: 'Sign your header with JWT',
 
-    const jwtSettings = context.request.getEnvironmentVariable(
-      PLUGIN_ENVIRONMENT_SETTINGS,
-    );
+  requestHooks: [
+    async context => {
+      console.log(`${PLUGIN_NAME} Running`);
 
-    const jwtHeaderName = jwtSettings?.['jwt-header-name'];
-    const jwtSecret = jwtSettings?.['jwt-secret'];
-    const jwtAlgorithm = jwtSettings?.['jwt-algorithm'] ?? 'HS256';
-    const jwtExpiresIn = jwtSettings?.['jwt-expiresIn'];
+      const jwtSettings = context.request.getEnvironmentVariable(
+        'jwt-generate-header',
+      );
 
-    if (!jwtSettings || !jwtHeaderName || !jwtSecret) {
-      cleanPluginHeader(context);
-      console.log(`${PLUGIN_NAME} Missing required settings`);
-      return;
-    }
+      const jwtHeaderName = jwtSettings?.['jwt-header-name'];
+      const jwtSecret = jwtSettings?.['jwt-secret'];
+      const jwtAlgorithm = jwtSettings?.['jwt-algorithm'] ?? 'HS256';
+      const jwtExpiresIn = jwtSettings?.['jwt-expiresIn'];
 
-    const jwtPayload = getPayload();
-    if (!jwtPayload) {
-      console.log(`${PLUGIN_NAME} No jwt-payload found`);
-      return;
-    }
+      if (!jwtSettings || !jwtHeaderName || !jwtSecret) {
+        cleanHeaders(context);
+        console.log(`${PLUGIN_NAME} Missing required parameters`);
+        return;
+      }
 
-    try {
-      let token = jwt.sign(JSON.parse(jwtPayload), jwtSecret, {
-        algorithm: jwtAlgorithm,
-        ...(jwtExpiresIn ? { expiresIn: jwtExpiresIn } : {}),
-      });
-      context.request.setHeader(jwtHeaderName, `${token}`);
-    } catch (error) {
-      console.log(`${PLUGIN_NAME} Error `, error);
-    } finally {
-      cleanPluginHeader(context);
-    }
-  },
-];
+      const jwtPayload = getPayload(context, jwtHeaderName);
+      if (!jwtPayload) {
+        console.log(`${PLUGIN_NAME} No jwt-payload found`);
+        return;
+      }
+
+      try {
+        let sig = jwt.sign(JSON.parse(jwtPayload), jwtSecret, {
+          algorithm: jwtAlgorithm,
+          ...(jwtExpiresIn ? { expiresIn: jwtExpiresIn } : {}),
+        });
+        context.request.setHeader(jwtHeaderName, `${sig}`);
+      } catch (error) {
+        console.log(`${PLUGIN_NAME} Error `, error);
+      } finally {
+        cleanHeaders(context);
+      }
+    },
+  ],
+};
